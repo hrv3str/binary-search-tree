ca048c3e734654c8206ece83b9117ea38f5a506e
hrv3str <r.novikov.zp.ua@gmail.com>
2023-09-28 11:52:37 +0300

    Implement 'build' button functionality: showing tree visualisation and showing stats for tree

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Tree from './binarySearchTree.js'
 import generateArray from './arrayGenerator.js'
 
 const arrayButton = document.getElementById('generator');
+const buildButton = document.getElementById('build-tree');
 
 const buffer = (() => {
     const bufferObject = {
@@ -11,16 +12,39 @@ const buffer = (() => {
     }
 
     const readArray = () => {
-        return bufferObject.tree.source;
+        return bufferObject.tree.data;
     }
 
     const setArray = (object) => {
         bufferObject.tree = object;
     }
 
+    const printTree = () => {
+        bufferObject.tree.print();
+    }
+
+    const treeStats = () => {
+
+        const levelOrder = bufferObject.tree.levelOrder();
+        const inOrder = bufferObject.tree.inorder();
+        const preOrder = bufferObject.tree.preorder();
+        const postOrder = bufferObject.tree.postorder();
+        const isBalanced = bufferObject.tree.isBalanced();
+
+        return {
+            isBalanced,
+            levelOrder,
+            inOrder,
+            preOrder,
+            postOrder,
+        }
+    }
+
     return {
         readArray,
-        setArray
+        setArray,
+        printTree,
+        treeStats
     }
 })();
 
@@ -28,6 +52,11 @@ const display = (() => {
     const arrayDisplay = document.getElementById('array-display');
     const arrayList = document.getElementById('delete-input');
     const consoleMock = document.getElementById('console');
+    const balancedLine = document.getElementById('is-balanced');
+    const levelOrderLine = document.getElementById('level-order');
+    const inOrderLine = document.getElementById('in-order');
+    const preOrderLine = document.getElementById('pre-order');
+    const postOrderLine = document.getElementById('post-order');
 
     const array = () => {
         const input = buffer.readArray();
@@ -48,10 +77,31 @@ const display = (() => {
     }
 
     const tree = () => {
-        const log = (message) => {
-            
+        balancedLine.textContent = '';
+        levelOrderLine.textContent = '';
+        inOrderLine.textContent = '';
+        preOrderLine.textContent = '';
+        postOrderLine.textContent = '';
+        consoleMock.innerHTML = '';
+        const backup = console.log;
+        const stats = buffer.treeStats();
+
+        const log = (text) => {
+            const body = document.createElement('code');
+            body.innerHTML = text.replace(/\b(\d{3})\b/g, '<span>$1</span>');
+            consoleMock.appendChild(body);
         }
+
+        console.log = log;
+        buffer.printTree();
+        console.log = backup;
+        balancedLine.textContent = stats.isBalanced;
+        levelOrderLine.textContent = `[${stats.levelOrder}]`;
+        inOrderLine.textContent = `[${stats.levelOrder}]`;
+        preOrderLine.textContent = `[${stats.preOrder}]`;
+        postOrderLine.textContent = `[${stats.postOrder}]`;
     }
+
     return {
         array,
         tree
@@ -65,5 +115,10 @@ const handleGenerator = () => {
     display.array();
 }
 
+const handleBuild = () => {
+    display.tree();
+}
+
 arrayButton.addEventListener('click', handleGenerator);
+buildButton.addEventListener('click', handleBuild);
 
