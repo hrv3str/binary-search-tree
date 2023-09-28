7ac7f3759bceb13faf5de1919873301d26ffe224
hrv3str <r.novikov.zp.ua@gmail.com>
2023-09-28 14:25:57 +0300

    Add function handling .insert() button

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import display from './display.js'
 
 const arrayButton = document.getElementById('generator');
 const buildButton = document.getElementById('build-tree');
+const insertButton = document.getElementById('insert');
 
 const buffer = (() => {
     const bufferObject = {
@@ -41,29 +42,58 @@ const buffer = (() => {
         }
     }
 
+    const insertNumber = (number) => {
+        bufferObject.tree.insert(number);
+    }
+
     return {
         readArray,
         setArray,
         printTree,
-        treeStats
+        treeStats,
+        insertNumber
     }
 })();
+
+const handleInsert = () => {
+    const input = document.getElementById('insert-input');
+    const data = buffer.readArray();
+    const number = parseInt(input.value, 10);
+    if (input.value === '') {
+        display.errorPromtInsert();
+        return;
+    } else if (number < 100) {
+        display.errorPromtTooSmall();
+        return;
+    } else if (number > 999) {
+        display.errorPromtTooBig();
+        return
+    } else if (data.includes(number)) {
+        display.errorPromtDuplicate();
+        return
+    } else {
+        buffer.insertNumber(number);
+        display.tree();
+    }
+}
 
 const handleGenerator = () => {
     const array = generateArray();
     const tree = new Tree(array)
     buffer.setArray(tree);
     display.array();
+    buildButton.addEventListener('click', handleBuild);
 }
 
 const handleBuild = () => {
     display.tree();
+    insertButton.addEventListener('click', handleInsert);
 }
 
 display.fadeUI();
+console.log(display)
 
 arrayButton.addEventListener('click', handleGenerator);
-buildButton.addEventListener('click', handleBuild);
 
 export default buffer;
 
